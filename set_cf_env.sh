0662a48a0209092f4dbb1dca2bc5dca27090f814
Adam King <rak@us.ibm.com>
2017-02-08 17:28:34 -0500

    sudo install cf
    Signed-off-by: Adam King <rak@us.ibm.com>

--- set_cf_env.sh
+++ set_cf_env.sh
@@ -6,6 +6,12 @@ echo "$TRAVIS_REPO_SLUG"
 echo "$TRAVIS_COMMIT"
 echo "$TRAVIS_COMMIT_RANGE"
 
+echo "download and install cloudfoundry cli"
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+sudo apt-get -y update
+sudo apt-get -y install cf-cli
+
 cf set-env $APP_NAME CF_REPO_SLUG $TRAVIS_REPO_SLUG
 cf set-env $APP_NAME CF_COMMIT $TRAVIS_COMMIT
 cf set-env $APP_NAME CF_COMMIT_RANGE $TRAVIS_COMMIT_RANGE
